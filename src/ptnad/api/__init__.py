3a14091e4e512e4009f6ae429d4c6d3963bb176f
Andrey Tyulenev <154950346+cloffyn@users.noreply.github.com>
2025-05-30 19:48:25 +0600

    bump version

--- src/ptnad/api/__init__.py
+++ src/ptnad/api/__init__.py
@@ -1,6 +1,9 @@
-from .bql import BQLAPI
-from .monitoring import MonitoringAPI
-from .replists import RepListsAPI
-from .signatures import SignaturesAPI
+from ptnad.api.bql import BQLAPI
+from ptnad.api.monitoring import MonitoringAPI
+from ptnad.api.replists import RepListsAPI
+from ptnad.api.signatures import SignaturesAPI
+from ptnad.api.sensors import SensorsAPI
+from ptnad.api.sources import SourcesAPI
+from ptnad.api.filters import FiltersAPI
 
-__all__ = ['BQLAPI', 'MonitoringAPI', 'RepListsAPI', 'SignaturesAPI']
+__all__ = ["BQLAPI", "MonitoringAPI", "RepListsAPI", "SignaturesAPI", "SensorsAPI", "SourcesAPI", "FiltersAPI"]
